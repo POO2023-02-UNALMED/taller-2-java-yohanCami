709475a07db02db531892e15f7806619e662a334
yohanCami <yosanchezm@unal.edu.co>
2023-08-20 18:11:07 -0500

    Corrección
    Corrección de condicionales (JAJAJA Obviamente si algo es null tampoco tiene atributo)

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -29,9 +29,13 @@ public class Auto {
 		if (this.registro == this.motor.registro) {
 			
 			for (int i = 0; i < this.asientos.length; i++) {
-				
-			    if(this.asientos[i].registro != this.registro) {
-			    	return "Las piezas no son originales";
+			    
+			    if(this.asientos[i] != null) {
+
+			    	if(this.asientos[i].registro != this.registro) {
+				    	return "Las piezas no son originales";
+				    }
+			    	
 			    }
 			}
 			
